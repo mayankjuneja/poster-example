be4d6fd259dbe1d32222527b430864660f83a2aa
Mayank Juneja <mayankjuneja1@gmail.com>
2013-01-26 20:38:16 +0530

    Added example

---
+++ example.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import urllib2, os, re
+from poster.encode import multipart_encode
+from poster.streaminghttp import register_openers
+
+INPUT_FILE_PATH = "" #File path
+INPUT_VAR_NAME  = "" #Form input variable name 
+URL             = "" #POST URL
+
+register_openers()
+
+datagen, headers = multipart_encode({INPUT_VAR_NAME : open(INPUT_FILE_PATH,"rb")})
+
+request = urllib2.Request(URL, datagen, headers)
+
+res = urllib2.urlopen(request).read()
